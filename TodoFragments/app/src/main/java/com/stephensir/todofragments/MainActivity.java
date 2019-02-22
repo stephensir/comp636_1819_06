72cac89d5cbcd5eb5db9fabcb5a1a02bda198945
Stephen <stephen@iMac-E1209587-9.local>
2019-02-22 19:05:05 +0800

    -update

---
+++ TodoFragments/app/src/main/java/com/stephensir/todofragments/MainActivity.java
@@ -0,0 +1,27 @@
+package com.stephensir.todofragments;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null){
+            TodoFragment todoFragment = new TodoFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, todoFragment)
+                    .commit();
+        }
+
+
+    }
+}
